4c6fd401c43f7eb31008b85ba6c8d5d8f7ef546c
Akira Uchihara <uchihara@tab.do>
2021-01-30 00:55:25 +0900

    Refactor: Add parse.go

---
+++ ugolang/parse.go
@@ -0,0 +1,76 @@
+package ugolang
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func matchToken(token, code string, idx int) (int, bool) {
+	tokenLen := len(token)
+	if idx + tokenLen + 1 > len(code) {
+		return 0, false
+	}
+	if code[idx : idx+tokenLen] != token {
+		return 0, false
+	}
+	nextChar := code[idx+tokenLen]
+	if '0' <= nextChar && nextChar <= '9' ||
+		 'a' <= nextChar && nextChar <= 'z' ||
+		 'A' <= nextChar && nextChar <= 'Z' ||
+		 nextChar == '_' {
+		return 0, false
+	}
+	return len(token)-1, true
+}
+
+func tokenize(code string) []Token {
+	tokens := make([]Token, 0)
+	for i := 0; i < len(code); i++ {
+		if matchLen, matched := matchToken("if", code, i); matched {
+			tokens = append(tokens, *NewToken(TokenIf))
+			i += matchLen
+			continue
+		}
+
+		if matchLen, matched := matchToken("else", code, i); matched {
+			tokens = append(tokens, *NewToken(TokenElse))
+			i += matchLen
+			continue
+		}
+
+		c := code[i]
+
+		if c == ' ' {
+			continue
+		}
+
+		if '0' <= c && c <= '9' {
+			var j int = i + 1
+			for ; '0' <= code[j] && code[j] <= '9' && j < len(code); j++ {
+			}
+			numStr := code[i:j]
+			num, err := strconv.ParseInt(numStr, 10, 64)
+			if err != nil {
+				panic(fmt.Sprintf("invalid num format: %s", numStr))
+			}
+			tokens = append(tokens, *NewNumToken(int(num)))
+			continue
+		}
+
+		if 'a' <= c && c <= 'z' {
+			tokens = append(tokens, *NewIdentToken(rune(c)))
+			continue
+		}
+
+		if c == '=' || c == '+' || c == '*' || c == '(' || c == ')' || c == '{' || c == '}' {
+			tokens = append(tokens, *NewSignToken(rune(c)))
+			continue
+		}
+
+		if c == ';' {
+			tokens = append(tokens, *NewToken(TokenEOL))
+			continue
+		}
+	}
+	return tokens
+}
--- ugolang/ugolang.go
+++ ugolang/ugolang.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"path/filepath"
 	"runtime"
-	"strconv"
 )
 
 type Ugolang struct {
@@ -35,76 +34,6 @@ func (u *Ugolang) Exec(code string) int {
 		ret = eval(&node)
 	}
 	return ret
-}
-
-func matchToken(token, code string, idx int) (int, bool) {
-	tokenLen := len(token)
-	if idx + tokenLen + 1 > len(code) {
-		return 0, false
-	}
-	if code[idx : idx+tokenLen] != token {
-		return 0, false
-	}
-	nextChar := code[idx+tokenLen]
-	if '0' <= nextChar && nextChar <= '9' ||
-		 'a' <= nextChar && nextChar <= 'z' ||
-		 'A' <= nextChar && nextChar <= 'Z' ||
-		 nextChar == '_' {
-		return 0, false
-	}
-	return len(token)-1, true
-}
-
-func tokenize(code string) []Token {
-	tokens := make([]Token, 0)
-	for i := 0; i < len(code); i++ {
-		if matchLen, matched := matchToken("if", code, i); matched {
-			tokens = append(tokens, *NewToken(TokenIf))
-			i += matchLen
-			continue
-		}
-
-		if matchLen, matched := matchToken("else", code, i); matched {
-			tokens = append(tokens, *NewToken(TokenElse))
-			i += matchLen
-			continue
-		}
-
-		c := code[i]
-
-		if c == ' ' {
-			continue
-		}
-
-		if '0' <= c && c <= '9' {
-			var j int = i + 1
-			for ; '0' <= code[j] && code[j] <= '9' && j < len(code); j++ {
-			}
-			numStr := code[i:j]
-			num, err := strconv.ParseInt(numStr, 10, 64)
-			if err != nil {
-				panic(fmt.Sprintf("invalid num format: %s", numStr))
-			}
-			tokens = append(tokens, *NewNumToken(int(num)))
-			continue
-		}
-
-		if 'a' <= c && c <= 'z' {
-			tokens = append(tokens, *NewIdentToken(rune(c)))
-			continue
-		}
-
-		if c == '=' || c == '+' || c == '*' || c == '(' || c == ')' || c == '{' || c == '}' {
-			tokens = append(tokens, *NewSignToken(rune(c)))
-			continue
-		}
-
-		if c == ';' {
-			tokens = append(tokens, *NewToken(TokenEOL))
-			continue
-		}
-	}
-	return tokens
 }
 
 func eval(node *Node) int {
