1b38d4cdee042d22c91498a22c090f237f845169
Akira Uchihara <uchihara@tab.do>
2021-01-30 00:58:19 +0900

    Refactor: Add util.go

--- ugolang/ugolang.go
+++ ugolang/ugolang.go
@@ -2,8 +2,6 @@ package ugolang
 
 import (
 	"fmt"
-	"path/filepath"
-	"runtime"
 )
 
 type Ugolang struct {
@@ -113,30 +111,6 @@ func expectSign(sign rune) {
 	if !consumeSign(sign) {
 		panic(fmt.Sprintf("expect %c but got %v", sign, tokens[0]))
 	}
-}
-
-func dprintf(f string, param ...interface{}) {
-	if true {
-		return
-	}
-	depth := 0
-	for i := 1; ; i++ {
-		_, _, _, ok := runtime.Caller(i)
-		if !ok {
-			break
-		}
-		depth++
-	}
-	pc, _, _, _ := runtime.Caller(1)
-	fn := runtime.FuncForPC(pc)
-	fileName, fileLine := fn.FileLine(pc)
-	fmt.Printf("%s:%d: ", filepath.Base(fileName), fileLine)
-
-	for i := 8; i < depth; i++ {
-		fmt.Printf(" ")
-	}
-
-	fmt.Printf(f, param...)
 }
 
 func prog() []Node {
---
+++ ugolang/util.go
@@ -0,0 +1,31 @@
+package ugolang
+
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+)
+
+func dprintf(f string, param ...interface{}) {
+	if true {
+		return
+	}
+	depth := 0
+	for i := 1; ; i++ {
+		_, _, _, ok := runtime.Caller(i)
+		if !ok {
+			break
+		}
+		depth++
+	}
+	pc, _, _, _ := runtime.Caller(1)
+	fn := runtime.FuncForPC(pc)
+	fileName, fileLine := fn.FileLine(pc)
+	fmt.Printf("%s:%d: ", filepath.Base(fileName), fileLine)
+
+	for i := 8; i < depth; i++ {
+		fmt.Printf(" ")
+	}
+
+	fmt.Printf(f, param...)
+}
